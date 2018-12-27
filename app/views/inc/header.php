ffd263735856bedf7ac87ba5b8771aeb3bd84ba8
Moix1 <abdulmoix1@outlook.com>
2018-12-27 15:30:14 +0500

    completed

---
+++ app/views/inc/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo SITENAME; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/style.css">
+</head>
+<body>
+    
